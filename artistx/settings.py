97ca71cd508da1f18e08de7460c2772454562c48
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-11-05 15:27:51 -0500

    Added requirements.txt

--- artistx/settings.py
+++ artistx/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
